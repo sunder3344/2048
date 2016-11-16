8e5b5c9e00d24d481a67170642c98b7814bd0a53
sunder3344 <sunder3344@gmail.com>
2016-11-16 23:15:25 +0800

    集成anysdk

---
+++ frameworks(anysdk)/runtime-src/proj.android/jni/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+$(call import-add-path,$(LOCAL_PATH)/../)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos2djs_shared
+
+LOCAL_MODULE_FILENAME := libcocos2djs
+
+LOCAL_SRC_FILES := hellojavascript/main.cpp \
+                   ../../Classes/AppDelegate.cpp \
+                   ../../Classes/jsb_anysdk_basic_conversions.cpp \
+                   ../../Classes/jsb_anysdk_protocols_auto.cpp \
+				   ../../Classes/manualanysdkbindings.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+
+LOCAL_STATIC_LIBRARIES := cocos2d_js_static
+
+LOCAL_WHOLE_STATIC_LIBRARIES := PluginProtocolStatic
+
+LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=2 -DCOCOS2D_JAVASCRIPT
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+$(call import-module, scripting/js-bindings/proj.android)
+
+$(call import-module,protocols/android)
